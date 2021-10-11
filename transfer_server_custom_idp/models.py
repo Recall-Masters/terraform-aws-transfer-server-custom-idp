b432cd5332186a76b381fd47da6732c5175cf4e8
Anatoly Scherbakov <altaisoft@gmail.com>
2021-10-11 12:45:59 +0700

    ETL-199 Insert missing alias of `server_id` → `serverId`

--- transfer_server_custom_idp/models.py
+++ transfer_server_custom_idp/models.py
@@ -1,11 +1,11 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Login(BaseModel):
     """Login request."""
 
     username: str
-    server_id: str
-    password: Optional[str] = None
+    server_id: str = Field(alias='serverId')
+    password: Optional[str]
