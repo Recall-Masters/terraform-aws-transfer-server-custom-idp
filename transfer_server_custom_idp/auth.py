121f38b36789fb36ea0d10657caaee8d64f05457
Anatoly Scherbakov <altaisoft@gmail.com>
2021-10-11 13:10:29 +0700

    ETL-199 CloudWatch log is not strictly JSON because of stdlib `logging` formatting

--- transfer_server_custom_idp/auth.py
+++ transfer_server_custom_idp/auth.py
@@ -61,7 +61,7 @@ def construct_response(
     logger: BoundLogger,
     bucket_name: str,
 ):
-    resp_data = {}
+    response = {}
 
     # It is recommended to verify server ID against some value, this template
     # does not verify server ID
@@ -69,22 +69,22 @@ def construct_response(
     input_password = login.password
 
     # Lookup user's secret which can contain the password or SSH public keys
-    resp = get_secret(
+    secret_configuration_string = get_secret(
         username=input_username,
         secrets_manager_prefix='SFTP',
         aws_region=os.environ['SECRETS_MANAGER_REGION'],
         logger=logger,
     )
 
-    if resp is not None:
-        resp_dict = json.loads(resp)
+    if secret_configuration_string is not None:
+        secret_configuration = json.loads(secret_configuration_string)
     else:
         logger.error("Secrets Manager exception thrown")
         return {}
 
     if input_password:
-        if 'password' in resp_dict:
-            resp_password = resp_dict['password']
+        if 'password' in secret_configuration:
+            resp_password = secret_configuration['password']
         else:
             logger.error(
                 'Unable to authenticate user - No field match in Secret for '
@@ -101,8 +101,8 @@ def construct_response(
         # SSH Public Key Auth Flow - The incoming password was empty
         # so we are trying ssh auth and need to return the public key data
         # if we have it
-        if 'PublicKey' in resp_dict:
-            resp_data['PublicKeys'] = [resp_dict['PublicKey']]
+        if 'PublicKey' in secret_configuration:
+            response['PublicKeys'] = [secret_configuration['PublicKey']]
         else:
             return {}
 
@@ -110,33 +110,33 @@ def construct_response(
     # or we're using SSH public key auth and
     # we've begun constructing the data response. Check for each key value pair.
     # These are required so set to empty string if missing
-    resp_data['Role'] = resp_dict.get('Role')
+    response['Role'] = secret_configuration.get('Role')
 
     # These are optional so ignore if not present
-    if 'Policy' in resp_dict:
-        resp_data['Policy'] = resp_dict['Policy']
+    if 'Policy' in secret_configuration:
+        response['Policy'] = secret_configuration['Policy']
 
     home_directory = generate_home_directory(
         template=home_directory_template,
-        secret=resp_dict,
+        secret=secret_configuration,
         user_name=input_username,
     )
 
-    if not resp_data['Role']:
-        resp_data['Role'] = os.getenv('DEFAULT_IAM_ROLE_ARN')
-        resp_data['Policy'] = json.dumps(construct_policy(
+    if not response['Role']:
+        response['Role'] = os.getenv('DEFAULT_IAM_ROLE_ARN')
+        response['Policy'] = json.dumps(construct_policy(
             bucket_name=bucket_name,
             home_directory=home_directory,
         ))
 
-    if 'HomeDirectoryDetails' in resp_dict:
+    if 'HomeDirectoryDetails' in secret_configuration:
         logger.error(
             "HomeDirectoryDetails found - Applying setting for virtual folders",
         )
-        resp_data['HomeDirectoryDetails'] = resp_dict['HomeDirectoryDetails']
+        response['HomeDirectoryDetails'] = secret_configuration['HomeDirectoryDetails']
 
-    resp_data['HomeDirectoryType'] = 'LOGICAL'
-    resp_data['HomeDirectoryDetails'] = json.dumps([{
+    response['HomeDirectoryType'] = 'LOGICAL'
+    response['HomeDirectoryDetails'] = json.dumps([{
         'Entry': '/',
         'Target': generate_absolute_path(
             home_directory=home_directory,
@@ -144,10 +144,10 @@ def construct_response(
         ),
     }])
 
-    if resp_data.get('HomeDirectory') is not None:
-        del resp_data['HomeDirectory']
+    if response.get('HomeDirectory') is not None:
+        del response['HomeDirectory']
 
-    return resp_data
+    return response
 
 
 def get_secret(
--- transfer_server_custom_idp/log.py
+++ transfer_server_custom_idp/log.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from typing import Optional
 
 import sentry_sdk
@@ -19,11 +20,29 @@ def create_logger(
             sample_rate=1.0,
         )
 
-    logging.basicConfig()
+    logging.basicConfig(
+        format="%(message)s",
+        stream=sys.stdout,
+        level=logging.INFO,
+    )
 
     structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.stdlib.render_to_log_kwargs,
+
+            structlog.processors.JSONRenderer(),
+        ],
+        context_class=dict,
         logger_factory=structlog.stdlib.LoggerFactory(),
-        processors=[structlog.processors.JSONRenderer()],
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
     )
 
     return structlog.get_logger()
