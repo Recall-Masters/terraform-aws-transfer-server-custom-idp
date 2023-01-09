fe169c03a051f10476a7b8ddbc872ecaca26f325
lgurnik <lgentts@gmail.com>
2023-01-09 17:04:08 +0100

    change to high level botocore client construction with session

--- transfer_server_custom_idp/s3_service/s3_handler_functions.py
+++ transfer_server_custom_idp/s3_service/s3_handler_functions.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import boto3
+from botocore.client import BaseClient
 
 from transfer_server_custom_idp.settings import (
     AWS_REGION, HOME_DIRECTORY_TO_FOLDERS_MAPPING,
@@ -35,11 +36,11 @@ def s3_path_existence_check(
 def create_folder_in_s3(
     bucket_name: str,
     folder_path: str,
+    s3_client: BaseClient,
 ) -> None:
     """Creates path in AWS S3 bucket without object upload."""
     try:
         logger.info("Folder path to create: %s", folder_path)
-        s3_client = boto3.client("s3")
         s3_client.put_object(
             Bucket=bucket_name,
             Body="",
@@ -56,6 +57,10 @@ def onboard_new_user_with_home_directory_folders_in_s3(
 ) -> None:
     """Creates the home directory folders based on home directory prefix."""
     logger.info("Onboard the: %s", home_directory)
+    s3_client = boto3.session.Session().client(
+        service_name="s3",
+        region_name=AWS_REGION,
+    )
     for mapping_key in HOME_DIRECTORY_TO_FOLDERS_MAPPING.keys():
         if mapping_key in home_directory and mapping_key != SFTP_COMPANY_PREFIX:
             for folder in HOME_DIRECTORY_TO_FOLDERS_MAPPING[mapping_key]:
@@ -63,6 +68,7 @@ def onboard_new_user_with_home_directory_folders_in_s3(
                 create_folder_in_s3(
                     bucket_name=bucket_name,
                     folder_path=f"{home_directory}/{folder}/",
+                    s3_client=s3_client,
                 )
         if SFTP_COMPANY_PREFIX in home_directory:
             for folder in HOME_DIRECTORY_TO_FOLDERS_MAPPING[SFTP_COMPANY_PREFIX]:
@@ -70,4 +76,5 @@ def onboard_new_user_with_home_directory_folders_in_s3(
                 create_folder_in_s3(
                     bucket_name=bucket_name,
                     folder_path=f"{home_directory.rsplit('/', 1)[0]}/{folder}/",
+                    s3_client=s3_client,
                 )
