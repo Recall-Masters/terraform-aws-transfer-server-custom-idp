d2db1ff5b37c2362b54870f0af267ad011438522
leonidhurniksyzygy <leonid.hurnik@syzygy.de>
2024-10-15 23:40:25 +0200

    RMBO24-111: Delete documented.

--- transfer_server_custom_idp/errors.py
+++ transfer_server_custom_idp/errors.py
@@ -1,23 +1,32 @@
-from dataclasses import dataclass
-
-from documented.error import DocumentedError
-
-
-@dataclass
-class UserNotFound(DocumentedError):
+class UserNotFound(Exception):
     """SFTP user {self.username} was not found in Secrets Manager."""
 
-    username: str
+    def __init__(self, username: str):
+        super().__init__(
+            f'SFTP user: "{username}" '
+            'was not found in Secrets Manager.',
+        )
 
 
-class IncorrectPassword(DocumentedError):
+class IncorrectPassword(Exception):
     """Provided password is incorrect."""
+    def __init__(self):
+        super().__init__(
+            "Provided password is incorrect.",
+        )
 
 
-class IncorrectUserConfiguration(DocumentedError):
+class IncorrectUserConfiguration(Exception):
     """Both `company_id` and `dealer_id` are not provided."""
+    def __init__(self):
+        super().__init__(
+            "Both `company_id` and `dealer_id` are not provided.",
+        )
 
 
-@dataclass
-class MissingCredentials(DocumentedError):
+class MissingCredentials(Exception):
     """Neither `password` nor `key` are provided."""
+    def __init__(self):
+        super().__init__(
+            "Neither `password` nor `key` are provided.",
+        )
